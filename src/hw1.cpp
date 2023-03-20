9b18d0856ef7bef78f0d235b2618207c329ee463
Vyacheslav Gorlov <vygorlov00@gmail.com>
2023-03-20 22:34:27 +0300

    corrected HW1

---
+++ src/hw1.cpp
@@ -0,0 +1,15 @@
+// first argument - path to artist file
+// second argument - path to gender file, third and further - artist name.
+
+#include <map>
+#include <string>
+#include <auxiliary.hpp>
+
+
+int main(int argc, char *argv[]) {
+    if (check_input(argc)) return 0;
+    std::string artist_name = get_artist_name(argc, argv);
+    std::map<std::string, std::string> gender_map = get_gender_map(argv[2]);
+    show_artist_info(argv[1], artist_name, gender_map);
+    return 0;
+}
